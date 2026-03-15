69574ccbcd47fd410fb3fff34ad9bf6dcd405807
woojinwhoisthebest <woojin7552@gmail.com>
2026-03-15 14:59:14 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,37 +1,15 @@
-mapboxgl.accessToken = "YOUR_MAPBOX_TOKEN"
+<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 
-const map = new mapboxgl.Map({
-container: "map",
-style: "mapbox://styles/mapbox/satellite-v9",
-center: [20,20],
-zoom: 1.5
-})
+<div id="map" style="height:600px"></div>
 
-let coins = 0
+<script>
+var map = L.map('map').setView([20, 0], 2);
 
-setInterval(()=>{
-coins++
-document.getElementById("coins").innerText = coins
-},10000)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19
+}).addTo(map);
 
-const cities = [
-{name:"Seoul",lat:37.5665,lng:126.9780},
-{name:"Tokyo",lat:35.6762,lng:139.6503},
-{name:"New York",lat:40.7128,lng:-74.0060},
-{name:"London",lat:51.5072,lng:-0.1276},
-{name:"Paris",lat:48.8566,lng:2.3522}
-]
-
-cities.forEach(city=>{
-
-const el = document.createElement("div")
-el.style.background = "red"
-el.style.width = "10px"
-el.style.height = "10px"
-el.style.borderRadius = "50%"
-
-new mapboxgl.Marker(el)
-.setLngLat([city.lng,city.lat])
-.addTo(map)
-
-})
+L.marker([37.5665,126.9780]).addTo(map)
+.bindPopup("Seoul");
+</script>
