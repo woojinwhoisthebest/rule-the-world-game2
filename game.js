7aa45213052c3e699f67b23234d6e2e8cb10e365
woojinwhoisthebest <woojin7552@gmail.com>
2026-03-15 15:06:25 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,39 @@
-<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
-<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-
-<div id="map" style="height:600px"></div>
-
-<script>
 var map = L.map('map').setView([20, 0], 2);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-maxZoom: 19
+    maxZoom: 19,
 }).addTo(map);
 
-L.marker([37.5665,126.9780]).addTo(map)
-.bindPopup("Seoul");
-</script>
+let coins = 0;
+
+setInterval(() => {
+    coins++;
+    document.getElementById("coins").innerText = coins;
+}, 10000);
+
+const cities = [
+    {name:"Seoul",lat:37.5665,lng:126.9780},
+    {name:"Tokyo",lat:35.6762,lng:139.6503},
+    {name:"New York",lat:40.7128,lng:-74.0060},
+    {name:"London",lat:51.5072,lng:-0.1276},
+    {name:"Paris",lat:48.8566,lng:2.3522}
+];
+
+cities.forEach(city => {
+
+    var marker = L.marker([city.lat, city.lng]).addTo(map);
+
+    marker.on("click", function(){
+
+        const nick = document.getElementById("nickname").value;
+
+        if(!nick){
+            alert("닉네임 입력하세요");
+            return;
+        }
+
+        marker.bindPopup(city.name + " - " + nick).openPopup();
+
+    });
+
+});
