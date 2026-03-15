6904b5fa2809100e8bcf6857e51bbc698c9bc895
woojinwhoisthebest <woojin7552@gmail.com>
2026-03-15 18:17:25 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,54 +1,8 @@
-// socket.io 서버 연결
+// 서버 주소
 const socket = io("https://rule-the-world-game2-production.up.railway.app");
 
-// GeoJSON 불러오기
-fetch("https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json")
-.then(res => res.json())
-.then(data => {
-  L.geoJSON(data, {
-    style: {
-      color: "blue",
-      weight: 1,
-      fillColor: "blue",
-      fillOpacity: 0.1
-    },
-    onEachFeature: function(feature, layer) {
-      const name = feature.properties.name;
-
-      layer.on("click", function() {
-        const nick = document.getElementById("nickname").value;
-        if (!nick) { alert("닉네임 입력"); return; }
-        if (coins < 50) { alert("코인 부족"); return; }
-
-        coins -= 50;
-        document.getElementById("coins").innerText = coins;
-
-        // 서버로 점령 요청 보내기
-        socket.emit("capture", { country: name, player: nick });
-      });
-
-      // 서버로부터 업데이트 받기
-      socket.on("update", (data) => {
-        if (data.countries[name]) {
-          const owner = data.countries[name].owner;
-          if (owner) {
-            layer.setStyle({
-              fillColor: getColor(owner),
-              color: getColor(owner),
-              fillOpacity: 0.35
-            });
-          } else {
-            layer.setStyle({ fillColor: "blue", color: "blue", fillOpacity: 0.1 });
-          }
-        }
-      });
-    }
-  }).addTo(map);
-});
-const socket = io("https://rule-the-world-game2-production.up.railway.app");
-
+// 지도 초기화
 var map = L.map('map').setView([20,0],2);
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
   maxZoom:19,
   noWrap:true
@@ -58,7 +12,7 @@ let coins = 0;
 let countriesData = {};
 let players = {};
 
-// 코인 1초 5개
+// 코인 증가 (1초 5개)
 setInterval(()=>{
   coins += 5;
   document.getElementById("coins").innerText = coins;
@@ -70,24 +24,25 @@ function getColor(name){
   let colors=["red","blue","green","purple","orange"];
   let hash=0;
   for(let i=0;i<name.length;i++){ hash+=name.charCodeAt(i); }
-  return colors[hash%colors.length];
+  return colors[hash % colors.length];
 }
 
 // GeoJSON 불러오기
 fetch("https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json")
-.then(res=>res.json())
-.then(data=>{
-  L.geoJSON(data,{
-    style:{
+.then(res => res.json())
+.then(data => {
+  L.geoJSON(data, {
+    style: {
       color:"blue",
       weight:1,
       fillColor:"blue",
       fillOpacity:0.1
     },
-    onEachFeature:function(feature,layer){
-      let name = feature.properties.name;
+    onEachFeature: function(feature, layer){
+      const name = feature.properties.name;
       countriesData[name] = {owner:null, price:50};
 
+      // 클릭 시 점령
       layer.on("click", function(){
         const nick = document.getElementById("nickname").value;
         if(!nick){ alert("닉네임 입력"); return; }
@@ -98,9 +53,10 @@ fetch("https://raw.githubusercontent.com/johan/world.geo.json/master/countries.g
         coins -= c.price;
         document.getElementById("coins").innerText = coins;
 
-        socket.emit("capture", {country:name, player:nick});
+        socket.emit("capture",{country:name, player:nick});
       });
 
+      // 서버 업데이트 반영
       socket.on("update", (data)=>{
         if(data.countries[name]){
           countriesData[name] = data.countries[name];
@@ -113,7 +69,7 @@ fetch("https://raw.githubusercontent.com/johan/world.geo.json/master/countries.g
           }
         }
 
-        // 랭킹
+        // 랭킹 표시
         if(data.players){
           players = data.players;
           let rankingHTML = "";
