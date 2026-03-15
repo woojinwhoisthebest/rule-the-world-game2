534617037ce8466598bf2504af24eaf20f59eaab
woojinwhoisthebest <woojin7552@gmail.com>
2026-03-15 16:01:52 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,39 +1,61 @@
-var map = L.map('map').setView([20, 0], 2);
+var map = L.map('map').setView([20,0],2);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+maxZoom:19
 }).addTo(map);
 
 let coins = 0;
 
-setInterval(() => {
-    coins++;
-    document.getElementById("coins").innerText = coins;
-}, 10000);
+setInterval(()=>{
+coins++;
+document.getElementById("coins").innerText = coins;
+},3000);
 
-const cities = [
-    {name:"Seoul",lat:37.5665,lng:126.9780},
-    {name:"Tokyo",lat:35.6762,lng:139.6503},
-    {name:"New York",lat:40.7128,lng:-74.0060},
-    {name:"London",lat:51.5072,lng:-0.1276},
-    {name:"Paris",lat:48.8566,lng:2.3522}
-];
+// 도시 1000개 생성
 
-cities.forEach(city => {
+const cities = [];
 
-    var marker = L.marker([city.lat, city.lng]).addTo(map);
+for(let i=0;i<1000;i++){
 
-    marker.on("click", function(){
+let lat = (Math.random()*140)-70;
+let lng = (Math.random()*360)-180;
 
-        const nick = document.getElementById("nickname").value;
+cities.push({
+name:"City"+i,
+lat:lat,
+lng:lng,
+owner:null
+});
 
-        if(!nick){
-            alert("닉네임 입력하세요");
-            return;
-        }
+}
 
-        marker.bindPopup(city.name + " - " + nick).openPopup();
+cities.forEach(city=>{
 
-    });
+var marker = L.circleMarker([city.lat, city.lng],{
+radius:4,
+color:"red",
+fillColor:"red",
+fillOpacity:0.8
+}).addTo(map);
+
+marker.on("click",function(){
+
+const nick = document.getElementById("nickname").value;
+
+if(!nick){
+alert("닉네임 입력하세요");
+return;
+}
+
+city.owner = nick;
+
+marker.setStyle({
+color:"blue",
+fillColor:"blue"
+});
+
+marker.bindPopup(city.name + " - " + nick).openPopup();
+
+});
 
 });
